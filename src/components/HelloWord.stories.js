86b24fbc251d6dcd80200f023db3a99e690520ba
Moro-Code <omar@omarnasr.ca>
2021-06-14 11:42:30 -0400

    initial commit

---
+++ src/components/HelloWord.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HelloWorld from "./HelloWorld";
+
+export default {
+  title: "Components/HelloWorld",
+  component: HelloWorld,
+};
+
+const Template = (args) => <HelloWorld {...args} />;
+
+export const Primary = Template.bind({});
