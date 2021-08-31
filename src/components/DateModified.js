b895f2c3141735c0b4a2e4b36c552bbe207f52f7
Kris Charbonneau <char0799@algonquinlive.com>
2021-08-31 16:30:00 -0400

    Added DateModified and HTMList components

---
+++ src/components/DateModified.js
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+export function DateModified(props) {
+  // TeamCity build dates are received in the format yyyyMMdd
+  const dateFormatted = props.date
+    ? props.date.replace(/^(.{4})(.{2})/gm, "$1-$2-")
+    : "NA";
+  return (
+    <dl className="mt-8 py-2 font-body font-normal text-sm">
+      <dt className="inline">{props.label}</dt>
+      <dd className="inline">
+        {dateFormatted === "NA" ? (
+          <time>{` ${dateFormatted}`}</time>
+        ) : (
+          <time dateTime={dateFormatted}>{` ${dateFormatted}`}</time>
+        )}
+      </dd>
+    </dl>
+  );
+}
+
+DateModified.defaultProps = {
+  label: "Date Modified: ",
+};
+
+DateModified.propTypes = {
+  /**
+   * Text to show before date, defaults to "Date Modified: "
+   */
+  label: PropTypes.string,
+
+  // Date string in format yyyyMMdd
+  date: PropTypes.string,
+};
