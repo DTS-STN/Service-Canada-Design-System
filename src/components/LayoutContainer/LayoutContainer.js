d7b3bc2cb3b347706b2cf3f6ed747f5e2fb0dd91
Caterina van Herpt <cvanherpt@deloitte.ca>
2021-11-19 15:16:32 -0500

    updating layout container class

--- src/components/LayoutContainer/LayoutContainer.js
+++ src/components/LayoutContainer/LayoutContainer.js
@@ -3,16 +3,16 @@ import PropTypes from "prop-types";
 
 /**
  * This utility container helps layout content across screen sizes according to
- * the canada-ca design system guidelines https://design.canada.ca/styles/layouts.html
+ * our TailwindCSS implementation of Bootstrap 5 breakpoints and maximum container widths.
  *
- * screen size 280px - full width, small margin applied
- * screen size 550px - max width 550px, padding removed
- * screen size 768px - max width 768px, margin left auto, margin right auto and padding applied
- * rest of the breakpoints - max width is the lower bound of the breakpoint
+ * You can use this container to wrap a page, section or component or you could simiply add
+ * the 'container' class to the top-most html element on your page, section or component.
+ *
+ *
  */
 export default function LayoutContainer(props) {
   return (
-    <div className={`${props.className ? props.className : ""}`}>
+    <div className={`container ${props.className ? props.className : ""}`}>
       {props.children}
     </div>
   );
