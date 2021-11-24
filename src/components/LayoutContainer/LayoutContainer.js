827cca7c307c11cf32fcaacce48985f06d306442
Caterina van Herpt <cvanherpt@deloitte.ca>
2021-11-24 15:05:56 -0500

    fixing typo

--- src/components/LayoutContainer/LayoutContainer.js
+++ src/components/LayoutContainer/LayoutContainer.js
@@ -5,13 +5,13 @@ import PropTypes from "prop-types";
  * This utility container helps to layout content across screen sizes according to
  * our TailwindCSS implementation of Bootstrap 3 breakpoints, maximum container widths and grids.
  *
- * You can use this container to wrap a page, section or component or you could simiply add
+ * You can use this container to wrap a page, section or component or you could simply add
  * the 'container' and 'bootstrap3-grid' classes to the top-most html element on your page, section or component.
  *
  * Note:
  *
  * This project does not contain the Bootstrap package so we do not have access to Bootstrap classes.
- * It simiply mimics some bootstrap configurations using TailwindCSS preset files.
+ * It simply mimics some bootstrap configurations using TailwindCSS preset files.
  *
  * For more information on Bootstrap 3 breakpoints, containers & grids visit this link:
  * https://getbootstrap.com/docs/3.4/css/#grid-media-queries
