2779b10a4db140dd1631a3a56f108d5a4fc1d143
Anthony <aalaimo@deloitte.ca>
2021-11-25 09:06:48 -0500

    removed random comments

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,9 +4,6 @@ import "../src/styles/forms.css";
 import "../src/styles/menu.css";
 import "../src/styles/globals.css";
 import "../src/icomoon/style.css";
-
-// [...]
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
