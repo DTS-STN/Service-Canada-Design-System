11acbdd65551057c5b06bde8c84a502ba4d4c355
Anthony <aalaimo@deloitte.ca>
2022-03-11 17:19:29 -0500

    updated prefix in styles pages

--- src/stories/styles/Dividers/Dividers.stories.js
+++ src/stories/styles/Dividers/Dividers.stories.js
@@ -12,19 +12,19 @@ export const Primary = Template.bind({});
 Primary.args = {
   children: (
     <>
-      <p className="text-browserh1 font-bold pb-4">Dividers</p>
+      <p className="ds-text-browserh1 ds-font-bold ds-pb-4">Dividers</p>
       <p>
         Note: Vertical dividers are intended to be used in the direction of
         rows. Horizontal dividers are intended to be used in the direction of
         columns If used incorrectly may lend to unexpected styling renders
       </p>
-      <p className="heading2">Horizontal Dividers</p>
-      <p className="heading3 p-4">Regular</p>
-      <div className="horizontal-regular m-4"></div>
-      <div className="bg-gray-200 p-4 m-4">
-        <p className="body-bold">class-name: horizontal-regular</p>
+      <p className="ds-heading2">Horizontal Dividers</p>
+      <p className="ds-heading3 ds-p-4">Regular</p>
+      <div className="ds-horizontal-regular ds-m-4"></div>
+      <div className="ds-bg-gray-200 ds-p-4 ds-m-4">
+        <p className="ds-body-bold">class-name: ds-horizontal-regular</p>
 
-        <p className="body">Styles:</p>
+        <p className="ds-body">Styles:</p>
         <ul>
           <li>border-color: grey60</li>
           <li>border-style: solid</li>
@@ -32,11 +32,11 @@ Primary.args = {
           <li>height: 0</li>
         </ul>
       </div>
-      <p className="heading3 p-4">Prominent</p>
-      <div className="horizontal-prominent m-4"></div>
-      <div className="bg-gray-200 p-4 m-4">
-        <p className="body-bold">class-name: horizontal-prominent</p>
-        <p className="body">Styles:</p>
+      <p className="ds-heading3 ds-p-4">Prominent</p>
+      <div className="ds-horizontal-prominent ds-m-4"></div>
+      <div className="ds-bg-gray-200 ds-p-4 ds-m-4">
+        <p className="ds-body-bold">class-name: ds-horizontal-prominent</p>
+        <p className="ds-body">Styles:</p>
         <ul>
           <li>border-color: grey85</li>
           <li>border-style: solid</li>
@@ -44,11 +44,11 @@ Primary.args = {
           <li>height: 0</li>
         </ul>
       </div>
-      <p className="heading3 p-4">Muted</p>
-      <div className="horizontal-muted m-4"></div>
-      <div className="bg-gray-200 p-4 m-4">
-        <p className="body-bold">class-name: horizontal-muted</p>
-        <p className="body">Styles:</p>
+      <p className="ds-heading3 ds-p-4">Muted</p>
+      <div className="ds-horizontal-muted ds-m-4"></div>
+      <div className="ds-bg-gray-200 ds-p-4 ds-m-4">
+        <p className="ds-body-bold">class-name: ds-horizontal-muted</p>
+        <p className="ds-body">Styles:</p>
         <ul>
           <li>border-color: grey40</li>
           <li>border-style: solid</li>
@@ -57,13 +57,13 @@ Primary.args = {
         </ul>
       </div>
 
-      <p className="heading2">Vertical Dividers</p>
-      <p className="heading3 p-4">Regular</p>
-      <div className="flex flex-rows m-4">
-        <div className="vertical-regular mr-4"></div>
-        <div className="bg-gray-200 flex-grow p-4">
-          <p className="body-bold">class-name: vertical-regular</p>
-          <p className="body">Styles:</p>
+      <p className="ds-heading2">Vertical Dividers</p>
+      <p className="ds-heading3 ds-p-4">Regular</p>
+      <div className="ds-flex ds-flex-rows ds-m-4">
+        <div className="ds-vertical-regular ds-mr-4"></div>
+        <div className="ds-bg-gray-200 ds-flex-grow ds-p-4">
+          <p className="ds-body-bold">class-name: ds-vertical-regular</p>
+          <p className="ds-body">Styles:</p>
           <ul>
             <li>border-color: grey40</li>
             <li>border-style: solid</li>
@@ -72,12 +72,12 @@ Primary.args = {
           </ul>
         </div>
       </div>
-      <p className="heading3 p-4">Prominent</p>
-      <div className="flex flex-row m-4">
-        <div className="vertical-prominent mr-4"></div>
-        <div className="bg-gray-200 p-4 flex-grow">
-          <p className="body-bold">class-name: vertical-prominent</p>
-          <p className="body">Styles:</p>
+      <p className="ds-heading3 ds-p-4">Prominent</p>
+      <div className="ds-flex ds-flex-row ds-m-4">
+        <div className="ds-vertical-prominent ds-mr-4"></div>
+        <div className="ds-bg-gray-200 ds-p-4 ds-flex-grow">
+          <p className="ds-body-bold">class-name: ds-vertical-prominent</p>
+          <p className="ds-body">Styles:</p>
           <ul>
             <li>border-color: grey40</li>
             <li>border-style: solid</li>
@@ -86,12 +86,12 @@ Primary.args = {
           </ul>
         </div>
       </div>
-      <p className="heading3 p-4">Muted</p>
-      <div className="flex flex-row m-4">
-        <div className="vertical-muted mr-4"></div>
-        <div className="bg-gray-200 p-4 flex-grow">
-          <p className="body-bold">class-name: vertical-muted</p>
-          <p className="body">Styles:</p>
+      <p className="ds-heading3 ds-p-4">Muted</p>
+      <div className="ds-flex ds-flex-row ds-m-4">
+        <div className="ds-vertical-muted ds-mr-4"></div>
+        <div className="ds-bg-gray-200 ds-p-4 ds-flex-grow">
+          <p className="ds-body-bold">class-name: ds-vertical-muted</p>
+          <p className="ds-body">Styles:</p>
           <ul>
             <li>border-color: grey40</li>
             <li>border-style: solid</li>
