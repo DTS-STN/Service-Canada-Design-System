fa3df7b2f4f92f8cf4991344d9407d5b7874c1cd
Anthony <aalaimo@deloitte.ca>
2022-03-11 13:10:15 -0500

    updated test cases

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -31,7 +31,7 @@ export function Link(props) {
       break;
     default:
       basicStyle =
-        "lg:ds-underline ds-text-multi-blue-blue70b ds-font-body ds-text-browserh5 ds-font-boldtext-mobileh5 ds-leading-33px hover:ds-text-multi-blue-blue50b";
+        "lg:ds-underline ds-text-multi-blue-blue70b ds-font-body ds-text-browserh5 ds-font-bold ds-text-mobileh5 ds-leading-33px hover:ds-text-multi-blue-blue50b";
       break;
   }
 
