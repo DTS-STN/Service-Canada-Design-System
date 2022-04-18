7abad11ba03aebf820a64457f07413d191282835
Anthony <aalaimo@deloitte.ca>
2022-04-18 13:46:32 -0400

    fixed pagination test case

--- src/components/Pagination/Pagination.test.js
+++ src/components/Pagination/Pagination.test.js
@@ -4,13 +4,13 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { axe, toHaveNoViolations } from "jest-axe";
-import { Default } from "./Pagination.stories.js";
+import { Mobile } from "./Pagination.stories.js";
 
 expect.extend(toHaveNoViolations);
 
 describe("Pagination Test", () => {
   it("has no a11y", async () => {
-    const { container } = render(<Default {...Default.args} />);
+    const { container } = render(<Mobile {...Mobile.args} />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
