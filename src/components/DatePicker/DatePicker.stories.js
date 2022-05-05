b1b086215a730f524442698f2983a07c3aa15a7f
Anthony <aalaimo@deloitte.ca>
2022-05-05 10:48:07 -0400

    date picker component

---
+++ src/components/DatePicker/DatePicker.stories.js
@@ -0,0 +1,17 @@
+import { DatePicker } from "./DatePicker";
+export default {
+  title: "Components/DatePicker",
+  component: DatePicker,
+};
+
+const Template = (args) => <DatePicker {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  id: "DatePicker",
+  startMin: "2010-03-27",
+  endMin: "2010-05-07",
+  startMax: "2010-03-27",
+  endMax: "2015-05-07",
+};
