aeda4226ac423cdd9edad41c7f98eb41528764b6
Anthony <aalaimo@deloitte.ca>
2022-05-12 10:24:56 -0400

    more information component

---
+++ src/components/MoreInfo/MoreInfo.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { axe, toHaveNoViolations } from "jest-axe";
+import { Default } from "./MoreInfo.stories.js";
+
+expect.extend(toHaveNoViolations);
+
+describe("MoreInfo component Test", () => {
+  it("has no a11y", async () => {
+    const { container } = render(<Default {...Default.args} />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
