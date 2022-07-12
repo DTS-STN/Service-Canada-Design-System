85134602a57bc5518dc3eb1839a9a127b56570bc
Anthony Alaimo <91564649+AnthonyA44@users.noreply.github.com>
2022-07-12 16:57:11 -0400

    add french translation to date picker component

--- src/components/DatePicker/DatePicker.stories.js
+++ src/components/DatePicker/DatePicker.stories.js
@@ -6,12 +6,39 @@ export default {
 
 const Template = (args) => <DatePicker {...args} />;
 
-export const Default = Template.bind({});
+export const English = Template.bind({});
 
-Default.args = {
+English.args = {
   id: "DatePicker",
   // hasYear: false,
   // hasDay: false,
+  lang: "en",
+  minYear: 1999,
+  maxYear: 2050,
+  hasError: true,
+  hasLabel: true,
+  formErrorProps: {
+    id: "formErrorId",
+    errorMessage: "This is how form error will be displayed",
+  },
+  formLabelProps: {
+    id: "requiredWithInfo",
+    label: "Required Label",
+    required: true,
+    infoText:
+      "Required label style with information icon. You can hide by clicking on icon again.",
+    helpText:
+      "Help text that is always visible under the label to provide users with primary information needed to fill in the form field. Limit of 2 sentences",
+  },
+};
+
+export const French = Template.bind({});
+
+French.args = {
+  id: "DatePicker",
+  // hasYear: false,
+  // hasDay: false,
+  lang: "fr",
   minYear: 1999,
   maxYear: 2050,
   hasError: true,
