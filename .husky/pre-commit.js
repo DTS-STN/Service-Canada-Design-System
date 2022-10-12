58fbf6ae55ae6f6f7e804625fc3b498bf770f234
Zain Syed <zeddotes@gmail.com>
2022-10-12 14:14:14 -0400

    - wtf

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -1,1 +1,17 @@
-process.exit(1);
+const chalk = require('chalk');
+const { exec } = require('./_/exec');
+
+const branchName = exec('git rev-parse --abbrev-ref HEAD', { trim: true });
+// check if this branch already exists in the remote
+const isInRemote = exec(`git show-branch remotes/origin/${branchName}`, { toString: false }).code === 0;
+
+if (!isInRemote) {
+  const validBranchPrefix = 'feature|fix|hotfix|chore|tests|automation';
+  const validBranchesRegex = new RegExp(`^(${validBranchPrefix})\/[\\w.-]+$`);
+
+  if (!validBranchesRegex.test(branchName)) {
+    const msg = `Branch names in this project must adhere to this contract: ${validBranchPrefix}.`
+    console.log(chalk.bgRed.black.bold(msg));
+    process.exit(1);
+  }
+}
