95131eea2eed9c21739ba27e67d67b9f27809d54
Jordan <jordan.walcaraz@gmail.com>
2022-11-09 11:32:18 -0800

    fix: changed alt text on warning icon

--- src/components/Message/Message.stories.js
+++ src/components/Message/Message.stories.js
@@ -46,7 +46,7 @@ Warning.args = {
   message_heading: "COVID-19 New services and service changes",
   message_body:
     "Find out about new support for Canadians during the COVID-19 pandemic and how Service Canada’s services are affectedFind out about new support for Canadians during the COVID-19 pandemic and how Service Canada’s services are affected",
-  alert_icon_alt_text: "warning icon",
+  alert_icon_alt_text: "warning",
   alert_icon_id: "warning icon",
 };
 
