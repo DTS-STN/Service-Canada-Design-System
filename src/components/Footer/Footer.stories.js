5695097881b7547ae4e88fb613597f4f8f10cbed
Shawn <azuneko1110@gmail.com>
2023-01-11 12:02:13 -0500

    feat: New props for footer links

--- src/components/Footer/Footer.stories.js
+++ src/components/Footer/Footer.stories.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Footer } from "./Footer";
+import { ENbrandLinks } from "../../translations/en.json";
+import { FRbrandLinks } from "../../translations/fr.json";
 
 export default {
   title: "Components/Footer",
@@ -14,6 +16,18 @@ EnglishAuth.args = {
   lang: "en",
   btnLink: "/",
   isAuthenticated: true,
+  brandLinks: [
+    {
+      id: "link1",
+      text: ENbrandLinks.link4,
+      href: ENbrandLinks.link4link,
+    },
+    {
+      id: "link2",
+      text: ENbrandLinks.link5,
+      href: ENbrandLinks.link5link,
+    },
+  ],
 };
 
 export const FrenchAuth = Template.bind({});
@@ -22,6 +36,18 @@ FrenchAuth.args = {
   lang: "fr",
   btnLink: "/",
   isAuthenticated: true,
+  brandLinks: [
+    {
+      id: "link1",
+      text: FRbrandLinks.link4,
+      href: FRbrandLinks.link4link,
+    },
+    {
+      id: "link2",
+      text: FRbrandLinks.link5,
+      href: FRbrandLinks.link5link,
+    },
+  ],
 };
 
 export const EnglishNonAuth = Template.bind({});
@@ -29,6 +55,33 @@ EnglishNonAuth.args = {
   id: "footer",
   lang: "en",
   btnLink: "/",
+  brandLinks: [
+    {
+      id: "link1",
+      text: ENbrandLinks.link1,
+      href: ENbrandLinks.link1link,
+    },
+    {
+      id: "link2",
+      text: ENbrandLinks.link2,
+      href: ENbrandLinks.link2link,
+    },
+    {
+      id: "link3",
+      text: ENbrandLinks.link3,
+      href: ENbrandLinks.link3link,
+    },
+    {
+      id: "link4",
+      text: ENbrandLinks.link4,
+      href: ENbrandLinks.link4link,
+    },
+    {
+      id: "link5",
+      text: ENbrandLinks.link5,
+      href: ENbrandLinks.link5link,
+    },
+  ],
 };
 
 export const FrenchNonAuth = Template.bind({});
@@ -36,6 +89,33 @@ FrenchNonAuth.args = {
   id: "footer",
   lang: "fr",
   btnLink: "/",
+  brandLinks: [
+    {
+      id: "link1",
+      text: FRbrandLinks.link1,
+      href: FRbrandLinks.link1link,
+    },
+    {
+      id: "link2",
+      text: FRbrandLinks.link2,
+      href: FRbrandLinks.link2link,
+    },
+    {
+      id: "link3",
+      text: FRbrandLinks.link3,
+      href: FRbrandLinks.link3link,
+    },
+    {
+      id: "link4",
+      text: FRbrandLinks.link4,
+      href: FRbrandLinks.link4link,
+    },
+    {
+      id: "link5",
+      text: FRbrandLinks.link5,
+      href: FRbrandLinks.link5link,
+    },
+  ],
 };
 
 export const ErrorPageFooter = Template.bind({});
