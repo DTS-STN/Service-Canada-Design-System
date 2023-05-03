6e6bf381a7eb34fce88f1231aa9fd1d6857339f0
Shawn <azuneko1110@gmail.com>
2023-05-03 16:56:50 -0400

    fix: import HelpIcon stories

--- src/components/HelpIcon/HelpIcon.test.tsx
+++ src/components/HelpIcon/HelpIcon.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { axe, toHaveNoViolations } from "jest-axe";
-import { Default } from "./HelpIcon.stories.js";
+import { Default } from "./HelpIcon.stories";
 import { HelpIcon } from "./HelpIcon.js";
 
 expect.extend(toHaveNoViolations);
