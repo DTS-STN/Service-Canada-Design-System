79defecef51441f580020449a632961583c33a24
Shawn <azuneko1110@gmail.com>
2023-05-15 13:53:09 -0400

    feat: added HintExpander component

---
+++ src/components/HintExpander/HintExpander.stories.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { HintExpander, HintExpanderProps } from "./HintExpander";
+import { Meta, Story } from "@storybook/react";
+export default {
+  title: "Components/HintExpander",
+  component: HintExpander,
+} as Meta<HintExpanderProps>;
+
+const Template: Story<HintExpanderProps> = (args) => <HintExpander {...args} />;
+
+export const Primary = Template.bind({});
+export const WithLink = Template.bind({});
+
+Primary.args = {
+  linkText: "Why are we asking this question?",
+  children: "We need to know this because your partner’s annual net income...",
+};
+
+WithLink.args = {
+  linkText: "Why are we asking this question?",
+  children: "We need to know this because your partner’s annual net income...",
+  withLink: true,
+  optionalLinkText: "For more information visit",
+  externalLinkText: "Link",
+};
