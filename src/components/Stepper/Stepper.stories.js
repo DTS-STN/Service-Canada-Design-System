b6b50d43f0967be9bedd59d47b2ede22ddb533ef
“Patrick“ <p@rickjobin.com>
2023-05-23 14:49:42 -0400

    feat: added next and previous buttons

--- src/components/Stepper/Stepper.stories.js
+++ src/components/Stepper/Stepper.stories.js
@@ -1,4 +1,5 @@
 import { Stepper } from "./Stepper";
+import { FormDatePicker } from "../FormDatePicker/FormDatePicker";
 
 export default {
   title: "Components/Stepper",
@@ -14,43 +15,34 @@ export const FooterBlueLink = Template.bind({});
 export const BreadcrumbsLink = Template.bind({});
 export const CardActionLink = Template.bind({});
 
+const filler = [
+  <>
+    <div className="ds-heading3">Date of Birth(required)</div>
+    <FormDatePicker />
+    <div>
+      At what age would you like to start receiving the OAS pension? (required)
+    </div>
+    <div>This should be between 65 and 70.</div>
+  </>,
+];
+
 Default.args = {
   id: "Stepper",
-  text: "Regular Stepper",
-  href: "/",
-};
-
-RegularLinkwithEmphasis.args = {
-  id: "link",
-  text: "Regular link with Emphasis",
-  href: "/",
-  linkStyle: "basicStyleWithEmphasis",
-};
-
-TitleLink.args = {
-  id: "link",
-  text: "Title Link",
-  href: "/",
-  linkStyle: "titleLink",
-};
-
-FooterBlueLink.args = {
-  id: "link",
-  text: "Small link - Footer blue",
-  href: "/",
-  linkStyle: "smfooterBlue",
-};
-
-BreadcrumbsLink.args = {
-  id: "link",
-  text: "Small link - Breadcrumbs & French toggle",
-  href: "/",
-  linkStyle: "smBreadcrumbs",
-};
-
-CardActionLink.args = {
-  id: "link",
-  text: "Card Action Link",
-  href: "/",
-  linkStyle: "cardActionLink",
+  name: "Benefits estimator:",
+  heading: "Step 1 of 4: Age",
+  previousProps: {
+    id: "Previous",
+    text: "Previous",
+    href: "#",
+    styling: "secondary",
+    iconAltText: "Icon Alt Text - Previous",
+  },
+  nextProps: {
+    id: "Next",
+    text: "Next",
+    href: "#",
+    styling: "primary",
+    iconAltText: "Icon Alt Text - Next",
+  },
+  children: filler,
 };
