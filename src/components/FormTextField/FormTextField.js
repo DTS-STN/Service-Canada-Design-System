aadaa8d4a03eacbab86de13575cc066d3530bc3b
Shawn <azuneko1110@gmail.com>
2023-06-12 15:51:46 -0400

    feat: new 12 character option for input field

--- src/components/FormTextField/FormTextField.js
+++ src/components/FormTextField/FormTextField.js
@@ -11,6 +11,42 @@ export function FormTextField(props) {
         value: props.value,
       }
     : {};
+
+  // Define a map of width-to-classname mappings
+  const width = {
+    standard: "ds-w-full ds-max-w-[408px]",
+    12: "ds-w-[166px]",
+    11: "ds-w-[154px]",
+    8: "ds-w-[120px]",
+    7: "ds-w-[109px]",
+    4: "ds-w-[74px]",
+    3: "ds-w-[63px]",
+    2: "ds-w-[51px]",
+  };
+
+  // Use the width-to-classname map to get the classname based on the selected width
+  const classname = `${
+    width[props.width] || props.width
+  } ds-rounded ds-text-input ds-text-mobileh5 ds-text-multi-neutrals-grey100 ds-min-h-44px ds-text-form-input-gray ds-border-2 ds-py-5px ds-px-14px ${
+    props.hasError
+      ? "ds-border-specific-red-red50b"
+      : "ds-border-multi-neutrals-grey85a focus:ds-border-multi-blue-blue60f"
+  } ${props.exclude ? "exclude" : ""}`;
+
+  // Define a map of width-to-maxLength mappings
+  const maxLengths = {
+    12: 12,
+    11: 11,
+    8: 8,
+    7: 7,
+    4: 4,
+    3: 3,
+    2: 2,
+  };
+
+  // Use the width-to-maxLength map to get the maxLength based on the selected width
+  const maxLength = maxLengths[props.width] || undefined;
+
   return (
     <div className={`ds-block ds-leading-tight ds-mb-10px`}>
       {props.label && (
@@ -28,43 +64,8 @@ export function FormTextField(props) {
         />
       )}
       <input
-        className={`${
-          props.size === "standard"
-            ? "ds-w-full ds-max-w-[408px]"
-            : props.size === "11"
-            ? "ds-w-[154px]"
-            : props.size === "8"
-            ? "ds-w-[120px]"
-            : props.size === "7"
-            ? "ds-w-[109px]"
-            : props.size === "4"
-            ? "ds-w-[74px]"
-            : props.size === "3"
-            ? "ds-w-[63px]"
-            : props.size === "2"
-            ? "ds-w-[51px]"
-            : props.size
-        } ds-rounded ds-text-input ds-text-mobileh5 ds-text-multi-neutrals-grey100 ds-min-h-44px ds-text-form-input-gray ds-border-2 ds-py-5px ds-px-14px ${
-          props.hasError
-            ? "ds-border-specific-red-red50b"
-            : "ds-border-multi-neutrals-grey85a focus:ds-border-multi-blue-blue60f"
-        } ${props.exclude ? "exclude" : ""}`}
-        maxLength={`
-          ${
-            props.size === "11"
-              ? "11"
-              : props.size === "8"
-              ? "8"
-              : props.size === "7"
-              ? "7"
-              : props.size === "4"
-              ? "4"
-              : props.size === "3"
-              ? "3"
-              : props.size === "2"
-              ? "2"
-              : ""
-          }`}
+        className={classname}
+        maxLength={maxLength}
         id={props.id}
         aria-describedby={props.describedBy}
         name={props.name}
@@ -86,7 +87,7 @@ export function FormTextField(props) {
 FormTextField.defaultProps = {
   value: "",
   type: "text",
-  size: "standard",
+  width: "standard",
 };
 
 FormTextField.propTypes = {
@@ -193,8 +194,8 @@ FormTextField.propTypes = {
   }),
 
   /**
-   * User can input one of the follow size option to apply
-   * to the input field, the default size is standard.
+   * User can input one of the follow width option to apply
+   * to the input field, the default width is standard.
    */
-  size: PropTypes.oneOf(["standard", "11", "8", "7", "4", "3", "2"]),
+  width: PropTypes.oneOf(["standard", "12", "11", "8", "7", "4", "3", "2"]),
 };
--- src/components/FormTextField/FormTextField.stories.js
+++ src/components/FormTextField/FormTextField.stories.js
@@ -25,7 +25,7 @@ Primary.args = {
     optionalLinkText: "",
     url: "",
   },
-  size: "standard",
+  width: "standard",
   uncontrolled: true,
   helpText:
     "Help text that is always visible under the label to provide users with primary information needed to fill in the form field. Limit of 2 sentences",
