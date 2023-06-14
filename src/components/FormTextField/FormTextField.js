3b567e58bf5b1b35fef52e63e36fc725e9b9baa7
Shawn <azuneko1110@gmail.com>
2023-06-14 11:16:15 -0400

    fix: adjust font on inputfield, clear text when width changed

--- src/components/FormTextField/FormTextField.js
+++ src/components/FormTextField/FormTextField.js
@@ -1,10 +1,8 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { FormError } from "../FormError/FormError";
 import { FormLabel } from "../FormLabel/FormLabel";
-/**
- * text field component
- */
+
 export function FormTextField(props) {
   const ifControlledProps = !props.uncontrolled
     ? {
@@ -12,7 +10,6 @@ export function FormTextField(props) {
       }
     : {};
 
-  // Define a map of width-to-classname mappings
   const width = {
     standard: "ds-w-full ds-max-w-[408px]",
     12: "ds-w-[166px]",
@@ -24,16 +21,14 @@ export function FormTextField(props) {
     2: "ds-w-[51px]",
   };
 
-  // Use the width-to-classname map to get the classname based on the selected width
   const classname = `${
     width[props.width] || props.width
-  } ds-rounded ds-text-input ds-text-mobileh5 ds-text-multi-neutrals-grey100 ds-min-h-44px ds-text-form-input-gray ds-border-2 ds-py-5px ds-px-14px ${
+  } ds-rounded ds-text-input ds-font-body ds-text-[20px] ds-leading-[33px] ds-text-mobileh5 ds-text-multi-neutrals-grey100 ds-min-h-44px ds-text-form-input-gray ds-border-2 ds-py-5px ds-px-14px ${
     props.hasError
       ? "ds-border-specific-red-red50b"
       : "ds-border-multi-neutrals-grey85a focus:ds-border-multi-blue-blue60f"
   } ${props.exclude ? "exclude" : ""}`;
 
-  // Define a map of width-to-maxLength mappings
   const maxLengths = {
     12: 12,
     11: 11,
@@ -44,8 +39,14 @@ export function FormTextField(props) {
     2: 2,
   };
 
-  // Use the width-to-maxLength map to get the maxLength based on the selected width
   const maxLength = maxLengths[props.width] || undefined;
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    // Clear the input field value when the width prop changes
+    inputRef.current.value = "";
+  }, [props.width]);
 
   return (
     <div className={`ds-block ds-leading-tight ds-mb-10px`}>
@@ -64,6 +65,7 @@ export function FormTextField(props) {
         />
       )}
       <input
+        ref={inputRef}
         className={classname}
         maxLength={maxLength}
         id={props.id}
@@ -91,100 +93,24 @@ FormTextField.defaultProps = {
 };
 
 FormTextField.propTypes = {
-  /**
-   * the id of the text field
-   */
   id: PropTypes.string.isRequired,
-
-  /**
-   * the name of the text field
-   */
   name: PropTypes.string.isRequired,
-
-  /**
-   * The textfield label
-   */
   label: PropTypes.string.isRequired,
-
-  /**
-   * The text which fills in the parenthesis, next to the label
-   */
   requiredText: PropTypes.string,
-
-  /**
-   * The text directly below the label text, used to give more information
-   * inregards to the given input
-   */
   helpText: PropTypes.string,
-
-  /**
-   * value of the text field
-   */
   value: PropTypes.string,
-
-  /**
-   * placeholder for the text field,
-   */
   placeholder: PropTypes.string,
-
-  /**
-   * the type of the input. Supports Number, Password, Email
-   */
   type: PropTypes.string,
-
-  /**
-   * call back for when the value of the text field changes
-   */
   onChange: PropTypes.func,
-
-  /**
-   * Check if filed has an error or not.
-   */
   hasError: PropTypes.bool,
-
-  /**
-   * boolean flag to specify that this input should be uncontrolled by react
-   */
   uncontrolled: PropTypes.bool,
-
-  /**
-   * min value allowed
-   */
   min: PropTypes.number,
-
-  /**
-   * max value allowed
-   */
   max: PropTypes.number,
-
-  /**
-   * the legal number of intervals
-   */
   step: PropTypes.number,
-
-  /**
-   * unit test selector
-   */
   dataTestId: PropTypes.string,
-
-  /**
-   * Exclude option for adding exclude class to the textfield
-   */
   exclude: PropTypes.bool,
-
-  /**
-   * aria-describedBy label id
-   */
   describedBy: PropTypes.string,
-
-  /**
-   * Option to show and custom Hint Expander
-   */
   hasHint: PropTypes.bool,
-
-  /**
-   * Hint Expander props
-   */
   hintProps: PropTypes.shape({
     textLink: PropTypes.string,
     description: PropTypes.string,
@@ -192,10 +118,5 @@ FormTextField.propTypes = {
     externalLinkText: PropTypes.string,
     optionalLinkText: PropTypes.string,
   }),
-
-  /**
-   * User can input one of the follow width option to apply
-   * to the input field, the default width is standard.
-   */
   width: PropTypes.oneOf(["standard", "12", "11", "8", "7", "4", "3", "2"]),
 };
