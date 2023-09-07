b4cae801490b1dd774bc2bc8b598784fbc53c02c
“Patrick“ <p@rickjobin.com>
2023-09-07 10:26:46 -0400

    fix:whole card focus

--- src/components/FormSelectableCard/FormSelectableCard.js
+++ src/components/FormSelectableCard/FormSelectableCard.js
@@ -31,32 +31,38 @@ export function FormSelectableCard(props) {
           <li
             className={`ds-min-h-[182px] ds-max-w-[360px] max-[574px]:ds-max-w-[290px] ds-rounded-lg hover:ds-shadow-[#0E62C9] ds-border-[1px] ${
               selected === id ? "ds-border-[#0E62C9]" : "ds-border-[#ACACAC]"
-            } hover:ds-border-[#0E62C9] hover:ds-drop-shadow-[0px_0px_6px_rgba(14,98,201,0.5)] ds-cursor-pointer ds-mb-8px`}
+            } hover:ds-border-[#0E62C9] hover:ds-drop-shadow-[0px_0px_6px_rgba(14,98,201,0.5)] ds-cursor-pointer ds-mb-8px ds-border-[1px] hover:ds-border-[2px]`}
             key={index}
             id={index}
           >
-            <UpperCard
-              {...props}
-              id={id}
-              label={label}
-              index={index}
-              validationClass={validationClass}
-              value={value}
-              selected={selected}
-              setSelected={setSelected}
-              checked={checked}
-            />
-            <LowerCard
-              {...props}
-              id={id}
-              label={label}
-              index={index}
-              validationClass={validationClass}
-              value={value}
-              selected={selected}
-              setSelected={setSelected}
-              checked={checked}
-            />
+            <label
+              htmlFor={id}
+              key={id + index}
+              className={`ds-radio ds-cursor-pointer`}
+            >
+              <UpperCard
+                {...props}
+                id={id}
+                label={label}
+                index={index}
+                validationClass={validationClass}
+                value={value}
+                selected={selected}
+                setSelected={setSelected}
+                checked={checked}
+              />
+              <LowerCard
+                {...props}
+                id={id}
+                label={label}
+                index={index}
+                validationClass={validationClass}
+                value={value}
+                selected={selected}
+                setSelected={setSelected}
+                checked={checked}
+              />
+            </label>
           </li>
         ))}
       </ul>
@@ -71,44 +77,38 @@ const UpperCard = (props) => {
     <div
       className={`ds-h-[72px] ${
         props.selected === props.id ? "ds-bg-[#F2F8FF]" : "ds-bg-[#F5F5F5]"
-      } ds-rounded-t-lg  ds-border-[1px] hover:ds-border-[2px] ds-border-b-[0px]`}
+      } ds-rounded-t-lg ds-border-[0px]`}
     >
-      <label
-        htmlFor={props.id}
-        key={props.id + props.index}
-        className={`ds-radio ds-cursor-pointer`}
-      >
-        <div className="ds-pt-[16px] ds-pt-[12px] ds-px-[24px] max-[574px]:ds-px-[16px] ds-flex ds-items-center">
-          <input
-            type="radio"
-            name={props.name}
-            id={props.id}
-            className="ds-hidden"
-            value={props.value}
-            tabIndex={0}
-            onChange={(e) => {
-              props.setSelected((prev) => props.id);
-              props.onChange(props.value, e);
-            }}
-            data-testid={`${props.id}-${props.dataTestId}`}
-            checked={props.selected === props.id}
-          />
-          <span
-            role="radio"
-            aria-label={props.name}
-            className={`ds-relative ds-label ds-block ds-mr-3.5 ds-min-w-43px ds-w-43px ds-h-43px ds-border-2 ds-border-solid ${props.validationClass} ds-rounded-full ds-float-left after:ds-absolute ds-outline-0`}
-            aria-checked={props.selected === props.id}
-            tabIndex={0}
-          ></span>
-          <p className="ds-heading5 ds-w-full">{props.value}</p>
+      <div className="ds-pt-[16px] ds-pt-[12px] ds-px-[24px] max-[574px]:ds-px-[16px] ds-flex ds-items-center">
+        <input
+          type="radio"
+          name={props.name}
+          id={props.id}
+          className="ds-hidden"
+          value={props.value}
+          tabIndex={0}
+          onChange={(e) => {
+            props.setSelected((prev) => props.id);
+            props.onChange(props.value, e);
+          }}
+          data-testid={`${props.id}-${props.dataTestId}`}
+          checked={props.selected === props.id}
+        />
+        <span
+          role="radio"
+          aria-label={props.name}
+          className={`ds-relative ds-label ds-block ds-mr-3.5 ds-min-w-43px ds-w-43px ds-h-43px ds-border-2 ds-border-solid ${props.validationClass} ds-rounded-full ds-float-left after:ds-absolute ds-outline-0`}
+          aria-checked={props.selected === props.id}
+          tabIndex={0}
+        ></span>
+        <p className="ds-heading5 ds-w-full">{props.value}</p>
 
-          <img
-            id={props.iconAlt}
-            src={props.icon || bank}
-            alt={props.iconAlt || "icon"}
-          />
-        </div>
-      </label>
+        <img
+          id={props.iconAlt}
+          src={props.icon || bank}
+          alt={props.iconAlt || "icon"}
+        />
+      </div>
     </div>
   );
 };
@@ -116,7 +116,7 @@ const UpperCard = (props) => {
 const LowerCard = (props) => {
   return (
     <div
-      className={`ds-min-h-[110px] ds-bg-white ds-rounded-b-lg ds-border-[1px] hover:ds-border-[2px] ds-border-t-[0px] ds-pl-[80px] max-[574px]:ds-pl-[72px] ds-pr-[24px] max-[574px]:ds-pr-[16px] ds-py-[12px]`}
+      className={`ds-min-h-[110px] ds-bg-white ds-rounded-b-lg ds-border-[0px] ds-pl-[80px] max-[574px]:ds-pl-[72px] ds-pr-[24px] max-[574px]:ds-pr-[16px] ds-py-[12px]`}
     >
       <p className="ds-card-body-text ds-font-normal">{props.label}</p>
     </div>
