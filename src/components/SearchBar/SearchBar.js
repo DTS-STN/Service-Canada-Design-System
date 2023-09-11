b0196ff89b1f3a54fbe385528e08d34b85d0acdc
“Patrick“ <p@rickjobin.com>
2023-09-11 10:54:34 -0400

    fix:nudge search icon

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -43,7 +43,7 @@ export function SearchBar(props) {
           type="submit"
         >
           <Image
-            className="ds-search-icon ds-ml-[2px]"
+            className="ds-search-icon ds-ml-[6px] ds-mt-[3px]"
             src={searchIcon}
             alt="Search Service Canada"
           />
